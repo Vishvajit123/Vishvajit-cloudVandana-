f631990929d6d1b67286e65d15e16e0b74a3ff13
Vishvajit Wani <137591186+Vishvajit123@users.noreply.github.com>
2023-10-22 17:14:41 +0530

    Add files via upload

--- Assignment2nd/progA.js
+++ Assignment2nd/progA.js
@@ -1,1 +1,11 @@
-
+function reverseWords(sentence) {
+    let words = sentence.split(" ");
+    let reversedWords = words.map(word => word.split("").reverse().join(""));
+    let reversedSentence = reversedWords.join(" ");
+    return reversedSentence;
+  }
+  
+  let sentence = "This is a sunny day";
+  let reversedSentence = reverseWords(sentence);
+  console.log(reversedSentence);
+  
---
+++ Assignment2nd/progB.js
@@ -0,0 +1,3 @@
+let array = [5, 2, 8, 1, 9];
+array.sort((a, b) => b - a);
+console.log(array);
